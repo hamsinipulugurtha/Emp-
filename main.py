68fb7f6f53158af67742132662110301d514d572
Laasya Kandukuri <sri25laasya@gmail.com>
2023-06-16 07:46:41 +0000

    Commit

--- main.py
+++ main.py
@@ -1,13 +1,35 @@
-from fastapi import FastAPI, UploadFile, File, Request, Form
-from fastapi.templating import Jinja2Templates
-from fastapi.responses import HTMLResponse
-from fastapi.staticfiles import StaticFiles
+from flask import Flask, render_template, request
+from werkzeug.utils import secure_filename
+import subprocess
 
-app = FastAPI()
-app.mount("/static", StaticFiles(directory="static"), name="static")
+app = Flask(__name__)
 
-templates = Jinja2Templates(directory="templates")
+# Route for the home page
+@app.route('/')
+def home():
+    return render_template('index.html')
 
-@app.get("/")
-async def dynamic_file(request: Request):
-    return templates.TemplateResponse("check.html", {"request": request})
+# Route for handling the file upload
+@app.route('/upload', methods=['POST'])
+def upload():
+    if 'file' not in request.files:
+        return 'No file uploaded'
+    
+    file = request.files['file']
+    if file.filename == '':
+        return 'No selected file'
+    
+    if file:
+        filename = secure_filename(file.filename)
+        file_path = 'uploads/' + filename
+        file.save(file_path)
+        
+        # Run the Python program with the uploaded video file
+        subprocess.run(['python', 'process_video.py', file_path])
+        
+        return 'File uploaded and processed successfully'
+    
+    return 'Error uploading file'
+
+if __name__ == '__main__':
+    app.run()
